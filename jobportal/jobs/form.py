2f5da43d81484f7f46347f55067bd36c8aea019d
Anjana <anjanachevara@gmail.com>
2022-01-20 14:26:14 +0530

    requirement file added

---
+++ jobportal/jobs/form.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Order
+
+
+class  OrderForm(ModelForm) :
+    class Meta :
+        model = Order
+        fields = '__all__'
+
